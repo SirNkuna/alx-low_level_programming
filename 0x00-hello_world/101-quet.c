11b1771865008dd57f4d0576032961db7d301bd9
SirNkuna <ernestnkuna7@gmail.com>
2023-06-15 17:02:52 -0700

    hello_world.c

---
+++ 0x00-hello_world/101-quet.c
@@ -0,0 +1,15 @@
+#!.bin/bash
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+* main - Entry point
+*
+* return: Alway 1 (success)
+*/
+int main(void)
+{     
+	write(2,
+	"and that piece of art is usefull\" - Dora korpar, 2015-10-19\n", 59),
+		retuern (1)
+}		
