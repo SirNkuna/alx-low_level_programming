4f26bd636fd8099e31fce42923943d5e27279d08
SirNkuna <ernestnkuna7@gmail.com>
2023-06-19 16:04:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+include <stdio.h
+/**
+ * main - a program that prints a line function
+ * Return: 0 9success
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
