c890c1e5596689bfb28b31ad473da00a3aca6f63
SirNkuna <ernestnkuna7@gmail.com>
2023-06-19 17:15:38 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unsistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-1o\n", 59);
+	return (1)
+}
